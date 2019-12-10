7a00d8ce53d1c4e8a020a4a29968169691813465
Robin Kwon <rovin_kr@naver.com>
2019-12-10 22:06:03 +0900

    신규 생성

---
+++ Assets/DiceRoller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DiceValues = new int[4];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int[] DiceValues;
+    public int DiceTotal;
+
+    public void RollTheDice()
+    {
+        DiceTotal = 0;
+        for (int i = 0; i < DiceValues.Length; i++)
+        {
+            DiceValues[i] = Random.Range(0, 2);
+            DiceTotal += DiceValues[i];
+        }
+        Debug.Log("Rolled: " + DiceTotal);
+    }
+}
